2fb395bc81a97ed13683d2fe2c4f76b04a9decb1
Nilesh Raj <56640529+nileshraj1988@users.noreply.github.com>
2021-05-31 17:46:36 +0530

    Update wp-config.php

--- wp-config.php
+++ wp-config.php
@@ -36,8 +36,8 @@ define( 'DB_CHARSET', 'utf8mb4' );
 
 /** The Database Collate type. Don't change this if in doubt. */
 define( 'DB_COLLATE', '' );
-define( 'WP_HOME', 'http://localhost/site' );
-define( 'WP_SITEURL', 'http://localhost/site' );
+define( 'WP_HOME', 'http://localhost:8888' );
+define( 'WP_SITEURL', 'https://nileshraj1988.github.io/' );
 /**#@+
  * Authentication Unique Keys and Salts.
  *
